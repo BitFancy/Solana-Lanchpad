b2c84838c39f6fc26c30e71f30e13f68ac45baee
BitFancy <alricbauer422031@gmail.com>
2023-05-04 14:47:51 +0530

    collection accept marketplace address as a parameter

--- pages/step2.js
+++ pages/step2.js
@@ -5,14 +5,20 @@ import Web3 from "web3";
 import marketplaceAbi from "../artifacts/contracts/FLOWMARKETPLACE/FlowMarketplace.sol/FlowMarketplace.json";
 import creatifyAbi from "../artifacts/contracts/FLOWCOLLECTION/FlowCollection.sol/FlowCollection.json";
 import { Messages } from "primereact/messages";
+import { withRouter } from "next/router";
+import { useEffect } from "react";
+import { FileUpload } from 'primereact/fileupload';
+
 // Contract Address: 0x96D6CAd0B0E9890225702f9dc202fB95618eA4E7
-export default function Step2() {
+const Step2=(props)=> {
   const msgs = useRef(null);
   const [marketplaceContarctA, setMarketplaceContarctA] = useState("");
   const [collectionContractA, setCollectionContractA] = useState("");
   const [_platformFee, setPlatformfee] = useState();
   const [contractName, setContractName] = useState("");
   const [contractSymbol, setcontractSymbol] = useState("");
+  const [uploadImage, setuploadImage] = useState("");
+
   var web3 = new Web3(Web3.givenProvider);
   const marketPlaceFlowContract = () => {
     const marketplaceContarct = new web3.eth.Contract(marketplaceAbi.abi);
@@ -64,76 +70,50 @@ export default function Step2() {
   const handleInputSymbol = (e) => {
     setcontractSymbol(e.target.value);
   };
+useEffect(() => {
+  setMarketplaceContarctA(props.router.query.contractAddress)
+}, [props.router.query.contractAddress])
+
+  console.log("Address in step 2", props.router.query.contractAddress);
   return (
     <div>
       <div className="card p-5 font-bold">
         Step 2 : Deploy Edition/Collection/AIREX/Subscription Nfts/Phygital NFTs{" "}
       </div>
-      <div className="flex justify-content-between gap-5">
-        <div className="card" style={{ width: "50%" }}>
-          <div className="flex p-2 justify-content-around">
-            <div>Enter marketplace Name</div>
-            <div>Enter marketplace fee</div>
-          </div>
-          <div className="flex p-2 justify-content-around gap-5">
-            <div>
-              <InputText
-                type="text"
-                name="contractName"
-                value={contractName}
-                onChange={handleInputName}
-                id="contractName"
-                className="p-2"
-                style={{ width: "100%" }}
-              />
-            </div>
-            <div>
-              <InputText
-                type="number"
-                name="_platformFee"
-                value={_platformFee}
-                onChange={handleInputFee}
-                className="p-2"
-              />
-            </div>
-          </div>
-          <div className="text-center mt-5">
-            <Button
-              label="Deploy FLow Marketplace"
-              onClick={marketPlaceFlowContract}
-              severity="Primary"
-              rounded
-            />
-          </div>
-          {msgs && <Messages ref={msgs} />}
-        </div>
+      <div className="flex justify-content-center gap-5">
+      
         <div className="card" style={{ width: "50%" }}>
           <div className="text-center mt-5">
-            <div>Enter Collection name</div>
+            <div className="mt-3 text-left">Enter Collection name</div>
             <div className="mt-3">
-              <InputText
+              <InputText 
                 value={contractName}
                 onChange={handleInputName}
-                className="p-2"
+                className="p-2 w-full"
                 type="text"
               />
             </div>
-            <div className="mt-3">Enter Collection Symbol</div>
+            <div className="mt-3 text-left">Enter Collection Symbol</div>
             <div className="mt-2">
               <InputText
                 value={contractSymbol}
                 onChange={handleInputSymbol}
-                className="p-2"
+                className="p-2 w-full"
                 type="text"
               />
             </div>
-            <div className="mt-3">Enter Marketplace address</div>
+            <div className="mt-3 text-left">Enter Marketplace address</div>
             <div className="mt-2">
               <InputText
                 value={marketplaceContarctA}
-                className="p-2"
+                className="p-2 w-full"
                 type="text"
+                disabled
               />
+            </div>
+            <div className="mt-5">
+            <FileUpload name="demo[]" url={'/api/upload'}  accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+
             </div>
           </div>
           <div className="text-center mt-5">
@@ -142,6 +122,7 @@ export default function Step2() {
               label="Deploy Collection"
               severity="Primary"
               rounded
+              disabled={!marketplaceContarctA}
             />
           </div>
         </div>
@@ -149,3 +130,6 @@ export default function Step2() {
     </div>
   );
 }
+
+
+export default withRouter(Step2)
