6f2ff455e2a43210c01d64d87c3a3543be55783b
BitFancy <alricbauer422031@gmail.com>
2023-05-19 22:58:20 +0530

    next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output:"export",
-  images:{
-    loader:'akamai',
-    path:"",
-  },
+  output:"standalone",
   assetPrefix: './'
 
 }
