132407ef08fd5059195b0cd36f7f2e12c5e064d3
BitFancy <alricbauer422031@gmail.com>
2023-05-19 23:35:11 +0530

    package changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output:"standalone",
+  output:"export",
+  images:{
+    loader:'akamai',
+    path:"",
+  },
   assetPrefix: './'
 
 }
