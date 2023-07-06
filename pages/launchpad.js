eb7cc1d26745bee6344bfc30cca6ee56f9975492
BitFancy <alricbauer422031@gmail.com>
2023-07-06 15:17:27 +0530

    Added launchpad page design as per the figma

--- pages/launchpad.js
+++ pages/launchpad.js
@@ -1,104 +1,141 @@
 import React from "react";
 import { Button } from "primereact/button";
-import Link from "next/link";
 import Layout from "../Components/Layout";
 export default function Launchpad() {
   return (
     <Layout title="Launchpad" description="Used to show launchpad information">
-    <div className="launchpad-back">
-      <div
-        className="text-center font-bold mt-16 text-3xl text-white"
-        style={{ top: "200px", position: "relative" }}
-      >
-        <div>Experience launching your favorites</div>
-        <div>categories in just few steps</div>
-      </div>
-      <div
-        className="flex justify-content-around card"
-        style={{ marginTop: "384px" }}
-      >
+      <div className="launchpad-back">
         <div
-          className=" text-white p-7 text-center card"
-          style={{ marginBottom: "0px",background:"black" }}
+          className="text-center font-bold mt-16 text-white"
+          style={{ top: "200px", position: "relative", fontSize: "50px" }}
         >
-          <div className="font-bold">SignatureSeries</div>
+          <div>Experience launching your favorites</div>
+          <div>categories in just few steps</div>
+        </div>
 
-          <div className="mt-5 text-sm">
-            {" "}
-            Myriadflow helps you to create your
-          </div>
-          <div>storefront unique, inviting, and</div>
-          <div>memorable for customers</div>
-          <div className="mt-5">
-            <Link href="/signatureseries">
-              <Button label="Launch SignatureSeries"  rounded style={{background:"white",color:"black"}}/>
-            </Link>
+       </div>
+      <div className="flex p-5 justify-content-around mt-5">
+        <div>
+          <div>
+            <div className="signature-series-head">SignatureSeries</div>
+            <div className="mt-5 text-3xl">
+              <div>Dive into SignatureSeries, an exclusive NFT </div>
+              <div>collection built on ERC-721, featuring one-of-a-</div>
+              <div>kind digital masterpieces. Immerse yourself in</div>
+              <div>the world of unique, non-fungible tokens, each </div>
+              <div>possessing a distinct, unreplicable signature of </div>
+              <div>authenticity."</div>
+            </div>
+            <div className="mt-5">
+              <Button label="Launch" rounded />
+            </div>
           </div>
         </div>
-        <div className=" text-white p-7 text-center card" style={{background:"black"}}>
-          <div className="font-bold">FusionSeries</div>
-          <div className="mt-5 text-sm">Watch your FusionSeries and saves </div>
-          <div>time buying one of them</div>
-
-          <div className="mt-5">
-          <Link href="/fusionSeries">
-            <Button label="Launch FusionSeries" style={{background:"white",color:"black"}} rounded />
-            </Link>
-          </div>
+        <div>
+          <img style={{ height: "300px" }} src="signatureseries.png"></img>
         </div>
       </div>
-      <div className="flex justify-content-around card">
-        <div
-          className="text-white p-7 text-center card"
-          style={{ marginBottom: "0px",background:"black" }}
-        >
-          <div className="font-bold">AIREX</div>
+      <div className="flex p-5 justify-content-around">
+        <div>
+          <div>
+            <img style={{ height: "300px" }} src="Fusionseries.png"></img>
+          </div>
+        </div>
+        <div>
+          <div className="signature-series-head">FusionSeries</div>
+          <div className="mt-5 text-3xl">
+            <div>Explore FusionSeries, a unique NFT collection </div>
+            <div>leveraging ERC-1155's power, creating </div>
+            <div>a seamless blend of unique and</div>
+            <div>interchangeable digital assets. Experience </div>
+            <div>pthe future of digital art, with bundled </div>
+            <div>collections and rich metadata that</div>
+            <div>add unprecedented depth and</div>
+            <div>flexibility to your NFT experience."</div>
+          </div>
 
-          <div className="mt-5 text-sm"> Dynamic NFTs that provide</div>
-          <div>dynamic features, changes </div>
-          <div>in a dNFT smart contract </div>
-          <div>are based on conditions.</div>
           <div className="mt-5">
-            <Link href="/deployflowaccessControl">
-              <Button label="Launch AIREX" style={{background:"white",color:"black"}} rounded />
-            </Link>
-          </div>
-        </div>
-        <div
-          className="text-white p-7 text-center card "
-          style={{ marginBottom: "0px",background:"black" }}
-        >
-          <div className="font-bold">EternumPass</div>
-          <div className="mt-5 text-sm"> An Opportunity to access</div>
-          <div>digital assets and </div>
-          <div>ownership to exclusive </div>
-          <div>content or services.</div>
-          <div className="mt-5">
-            <Link href="/eternumPass">
-              <Button
-                label="Launch EternumPass"
-                style={{background:"white",color:"black"}}
-                rounded
-              />
-            </Link>
-          </div>
-        </div>
-        <div
-          className="text-white p-7 text-center card"
-          style={{ marginBottom: "0px",background:"black" }}
-        >
-          <div className="font-bold">Phygital NFTs</div>
-          <div className="mt-5 text-sm"> Experience both the</div>
-          <div>Phygital and digital worlds </div>
-          <div>with myriadflow</div>
-          <div className="mt-5">
-            <Link href="/deployflowaccessControl">
-              <Button label="Launch Phygital NFTs" style={{background:"white",color:"black"}} rounded />
-            </Link>
+            <Button label="Launch" rounded />
           </div>
         </div>
       </div>
-    </div>
+      <div className="flex p-5 justify-content-around mt-5">
+        <div>
+          <div>
+            <div className="signature-series-head">Instagen</div>
+            <div className="mt-5 text-3xl">
+              <div>Discover InstaGen, an innovative NFT collection </div>
+              <div>underpinned by the ERC-721A protocol,</div>
+              <div>offering unique, indivisible, and limited-</div>
+              <div>edition digital assets. Each piece stands as a </div>
+              <div>testament to rarity and uniqueness, </div>
+              <div>enhancing the value and exclusivity of your."</div>
+              <div>digital art portfolio."</div>
+            </div>
+            <div className="mt-5">
+              <Button label="Launch" rounded />
+            </div>
+          </div>
+        </div>
+        <div>
+          <img style={{ height: "300px" }} src="instagen.png"></img>
+        </div>
+      </div>
+      <div className="flex p-5 justify-content-around mt-5">
+        <div>
+          <div>
+            <img style={{ height: "300px" }} src="dynamicrealms.png"></img>
+          </div>
+        </div>
+        <div>
+          <div className="signature-series-head">DynamicRealms</div>
+          <div className="mt-5 text-3xl">
+            <div>Dynamic NFTs that provide dynamic </div>
+            <div>features, changes in a dNFT smart </div>
+            <div>contract are based on conditions.</div>
+          </div>
+          <div className="mt-5">
+            <Button label="Launch" rounded />
+          </div>
+        </div>
+      </div>
+      <div className="flex p-5 justify-content-around mt-5">
+        <div>
+          <div>
+            <div>
+              <div className="signature-series-head">EternumPass</div>
+              <div className="mt-5 text-3xl">
+                <div>An Opportunity to access digital assets</div>
+                <div>and ownership to exclusive content </div>
+                <div>or services.</div>
+              </div>
+              <div className="mt-5">
+                <Button label="Launch" rounded />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img style={{ height: "300px" }} src="Eternumpass.png"></img>
+        </div>
+      </div>
+      <div className="flex p-5 justify-content-around mt-5">
+        <div>
+          <div>
+            <img style={{ height: "300px" }} src="Phygital.png"></img>
+          </div>
+        </div>
+        <div>
+          <div className="signature-series-head">Phygital NFTs</div>
+          <div className="mt-5 text-3xl">
+            <div>Experience both the Phygital and </div>
+            <div>digital worlds with myriadflow</div>
+          </div>
+          <div className="mt-5">
+            <Button label="Launch" rounded />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
