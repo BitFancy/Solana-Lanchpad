551e7a5b1628c835bcd470e57aff92e423852cef
BitFancy <alricbauer422031@gmail.com>
2023-07-19 17:21:21 +0530

    changes

--- pages/api/oauth/twitter.js
+++ pages/api/oauth/twitter.js
@@ -1,52 +1,3 @@
-//     // Fetch the user data using the access token
-//     const userData = await axios.get('https://api.twitter.com/1.1/account/verify_credentials.json', {
-//       headers: {
-//         Authorization: `Bearer ${response.data.access_token}`,
-//       },
-//     });
-
-
-
-
-
-
-
-// import axios from 'axios';
-
-// const TWITTER_CLIENT_ID = 'f2HoWwaWN1fbzJbrpdGupEJcc';
-// const TWITTER_CLIENT_SECRET = '2FbIz2Io0bF5QHL9W8s9NguulrS9waUGGnkpdnjIJMBkOZrgvN';
-
-// export default async function handler(req, res) {
-//   try {
-//     const credentials = Buffer.from(`${TWITTER_CLIENT_ID}:${TWITTER_CLIENT_SECRET}`).toString('base64');
-
-//     const requestBody = new URLSearchParams();
-//     requestBody.append('grant_type', 'client_credentials');
-
-//     const response = await axios.post(
-//       'https://api.twitter.com/oauth2/token',
-//       requestBody.toString(),
-//       {
-//         headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//           'Authorization': `Basic ${credentials}`,
-//         },
-//       }
-//     );
-
-//     const { access_token } = response.data;
-//     // Use the access_token for authenticated requests to the Twitter API
-
-//     res.status(200).json({ access_token });
-
-//   } catch (error) {
-//     console.error('Failed to obtain Twitter API bearer token:', error);
-//     res.status(500).json({ error: 'Failed to authenticate with Twitter' });
-//   }
-// }
-
-
-
 import axios from 'axios';
 import { generateCodeVerifier, generateCodeChallenge } from '../../../utils/pkceUtils';
 import OAuth from 'oauth-1.0a';
@@ -63,7 +14,7 @@ const CODE_VERIFIER = codeVerifier; // Replace with your code verifier
 
 const consumer_key = 'f2HoWwaWN1fbzJbrpdGupEJcc';
 const consumer_secret = '2FbIz2Io0bF5QHL9W8s9NguulrS9waUGGnkpdnjIJMBkOZrgvN';
-const requestTokenURL = 'https://api.twitter.com/oauth/request_token?oauth_callback=oob';
+const requestTokenURL = 'https://api.twitter.com/oauth/request_token';
 const endpointURL = `https://api.twitter.com/2/users/me?{params}`;
 
 
@@ -79,7 +30,8 @@ const oauth = OAuth({
 async function requestToken() {
   const authHeader = oauth.toHeader(oauth.authorize({
     url: requestTokenURL,
-    method: 'POST'
+    method: 'POST',
+    data: { oauth_callback: REDIRECT_URI },
   }));
 
   try {
@@ -97,47 +49,6 @@ async function requestToken() {
   } catch (error) {
     console.error('Failed to obtain Twitter OAuth request token:', error);
     throw new Error('Cannot get an OAuth request token');
-  }
-}
-
-async function exchangeTokens(oauthToken, oauthTokenSecret, pin) {
-  const accessTokenURL = 'https://api.twitter.com/oauth/access_token';
-
-  const token = {
-    key: oauthToken,
-    secret: oauthTokenSecret
-  };
-
-  const authHeader = oauth.toHeader(oauth.authorize({
-    url: accessTokenURL,
-    method: 'POST',
-    data: { oauth_verifier: pin }
-  }, token));
-
-  const req = await got.post(accessTokenURL, {
-    headers: {
-      Authorization: authHeader["Authorization"],
-      'Content-Type': 'application/json'
-    },
-    form: {
-      oauth_verifier: pin
-    }
-  });
-
-  if (req.body) {
-    const parsedBody = qs.parse(req.body);
-    const accessToken = parsedBody.oauth_token;
-    const accessTokenSecret = parsedBody.oauth_token_secret;
-
-    // Store the user's access tokens securely for future use
-    // Use accessToken and accessTokenSecret for authenticated requests
-
-    console.log('Access Tokens:', {
-      accessToken,
-      accessTokenSecret
-    });
-  } else {
-    throw new Error('Failed to exchange tokens');
   }
 }
 
@@ -199,17 +110,8 @@ export default async function handler(req, res) {
     // Use the access_token for authenticated requests to the Twitter API
     const oAuthRequestToken = await requestToken();
 
-    const authorizationURL = `https://api.twitter.com/oauth/authenticate?oauth_token=${oAuthRequestToken.oauth_token}`;
-    // res.redirect(authorizationURL);
-
-    // Pass oAuthRequestToken to the redirect URL as a query parameter
-    res.redirect(`/pinpage?oAuthToken=${oAuthRequestToken.oauth_token}&oAuthTokenSecret=${oAuthRequestToken.oauth_token_secret}&authURL=${authorizationURL}`);
-
-    // await exchangeTokens(
-    //   oAuthRequestToken.oauth_token,
-    //   oAuthRequestToken.oauth_token_secret,
-    //   pin
-    // );
+    const authorizationURL = `https://api.twitter.com/oauth/authorize?oauth_token=${oAuthRequestToken.oauth_token}`;
+    res.redirect(authorizationURL)
 
     res.status(200).json({ access_token, oAuthRequestToken});
 
